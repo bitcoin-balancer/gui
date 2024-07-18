1567ffa16ac018889d6bd7fc966b9fe1eaa10ff0
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-07-18 10:56:24 -0400

    Fix shadcn's setup
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/app.component.tsx
+++ src/pages/app.component.tsx
@@ -1,7 +1,9 @@
 import { useCallback, useState } from 'react';
+import { EnvelopeOpenIcon, ReloadIcon } from '@radix-ui/react-icons';
 import { ENVIRONMENT } from '../environment/environment.ts';
 import { Altcha } from '../shared/components/altcha/altcha.component.tsx';
-import { Button } from '@/shadcn/components/ui/button';
+
+import { Button } from '@/shared/shadcn/components/ui/button.tsx';
 
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
@@ -21,7 +23,11 @@ const App = () => {
       <p>{ENVIRONMENT.version}</p>
       <button className='px-5 py-3 hover:bg-slate-200' onClick={() => setCount(count + 1)}>+1</button>
       <br/><br/>
-      <Button>Click me</Button>
+      <Button><EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email</Button>
+    <Button disabled>
+      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+      Please wait
+    </Button>
       <br/><br/>
       <Altcha onVerified={handleAltchaVerification}/>
     </>
