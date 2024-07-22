313678a6193e8e1ddcb7e2d6113be7ae44849cdc
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-07-22 12:28:59 -0400

    Make the section fully responsive
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/sign-in/index.component.tsx
+++ src/pages/sign-in/index.component.tsx
@@ -17,7 +17,7 @@ const SignIn = () => {
   return (
     <main className='flex h-dvh'>
 
-      <section className='flex-1 bg-primary p-10'>
+      <section className='hidden md:block flex-1 bg-primary shadow-2xl p-10'>
 
         <article className='flex flex-col h-full'>
           <img src='logo/logo-light.png' alt='Balancer Logo' width='192' height='60' className='w-52' />
@@ -25,10 +25,7 @@ const SignIn = () => {
           <span className='my-auto'></span>
 
           <blockquote className='text-white self-end'>
-            <p className='text-2xl'>
-              "Bitcoin is a remarkable cryptographic achievement, and the ability to create 
-              something that is not duplicable in the digital world has enormous value."
-            </p>
+            <p className='text-2xl'> "Bitcoin is a remarkable cryptographic achievement, and the ability to create something that is not duplicable in the digital world has enormous value."</p>
             <p className='text-sm mt-3'>Eric Schmidt, CEO of Google</p>
           </blockquote>
 
@@ -36,13 +33,11 @@ const SignIn = () => {
 
       </section>
 
-      <section className='flex-1 self-center p-10'>
+      <section className='flex-1 self-center p-5 sm:p-10'>
 
-        <article className='w-3/6 mx-auto'>
+        <article className='w-full sm:w-10/12 md:11/12 lg:w-9/12 xl:w-7/12 2xl:w-6/12 mx-auto'>
           <h1 className='text-3xl font-bold text-center'>Sign In</h1>
-          <p className='text-light text-md text-center'>
-            Enter your credentials to log into your account
-          </p>
+          <p className='text-light text-md text-center'>Enter your credentials to log into your account</p>
 
           <div className='mt-5'>
             <Label htmlFor='nickname'>Nickname</Label>
@@ -68,10 +63,7 @@ const SignIn = () => {
 
           <Button type='submit' variant='outline' className='mt-3 w-full'>Update Password</Button>
 
-          <p className='text-light text-sm mt-6 text-center'>
-            If this is the first time you are signing into your account, make sure to set a password
-            on it by going through the <Link to='/update-password'><strong>"Update Password"</strong></Link> functionality.
-          </p>
+          <p className='text-light text-sm mt-6 text-center'>If this is the first time you are signing into your account, go through the <Link to='/update-password'><strong>"Update Password"</strong></Link> section to set a password on it.</p>
 
         </article>
 
