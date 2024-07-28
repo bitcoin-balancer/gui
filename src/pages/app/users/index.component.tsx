71049a4cf39f2045fc68ad7d6644438408d7d073
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-07-28 12:30:19 -0400

    Move user row to its own component
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/app/users/index.component.tsx
+++ src/pages/app/users/index.component.tsx
@@ -1,35 +1,19 @@
-import {
-  UserPlus,
-  EllipsisVertical,
-  UserPen,
-  UserMinus,
-  KeyRound,
-  SquareAsterisk,
-} from 'lucide-react';
+import { UserPlus } from 'lucide-react';
 import { Button } from '../../../shared/shadcn/components/ui/button.tsx';
 import {
   Tooltip,
   TooltipTrigger,
   TooltipContent,
 } from '../../../shared/shadcn/components/ui/tooltip.tsx';
-import { Badge } from '../../../shared/shadcn/components/ui/badge.tsx';
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuItem,
-} from '../../../shared/shadcn/components/ui/dropdown-menu.tsx';
 import {
   Table,
   TableBody,
   TableCaption,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '../../../shared/shadcn/components/ui/table.tsx';
+import UserRow from './user-row.component.tsx';
 
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
@@ -74,54 +58,7 @@ const Users = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell>
-                <p className='mr-2 font-bold'>root</p>
-              </TableCell>
-              <TableCell>
-                <Badge variant='secondary'>5</Badge>
-              </TableCell>
-              <TableCell>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button variant='ghost' size='sm' className='max-w-20 md:max-w-24 lg:max-w-32 xl:max-w-36 2xl:max-w-40'>
-                      <p className='text-ellipsis overflow-hidden font-normal'>6234ca63-426e-472c-9440-5e0b30640a5f</p>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Click to copy</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TableCell>
-              <TableCell>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button variant='ghost' size='sm' className='max-w-20 md:max-w-24 lg:max-w-32 xl:max-w-36 2xl:max-w-40'>
-                      <p className='text-ellipsis overflow-hidden font-normal'>PJWC6KIPJUWHMAKC</p>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Click to copy</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger aria-label='User actions menu'><EllipsisVertical aria-hidden='true'/></DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuLabel>root</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem><UserPen aria-hidden='true' className='w-5 h-5 mr-1' /> Update nickname</DropdownMenuItem>
-                    <DropdownMenuItem><UserPen aria-hidden='true' className='w-5 h-5 mr-1' /> Update authority</DropdownMenuItem>
-                    <DropdownMenuItem><UserPen aria-hidden='true' className='w-5 h-5 mr-1' /> Update OTP secret</DropdownMenuItem>
-                    <DropdownMenuItem><UserMinus aria-hidden='true' className='w-5 h-5 mr-1' /> Delete user</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem><KeyRound aria-hidden='true' className='w-5 h-5 mr-1' /> Display auth sessions</DropdownMenuItem>
-                    <DropdownMenuItem><SquareAsterisk aria-hidden='true' className='w-5 h-5 mr-1' /> Display password updates</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
+            <UserRow />
           </TableBody>
         </Table>
 
---
+++ src/pages/app/users/user-row.component.tsx
@@ -0,0 +1,96 @@
+import {
+  EllipsisVertical,
+  UserPen,
+  UserMinus,
+  KeyRound,
+  SquareAsterisk,
+} from 'lucide-react';
+import { Button } from '../../../shared/shadcn/components/ui/button.tsx';
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from '../../../shared/shadcn/components/ui/tooltip.tsx';
+import { Badge } from '../../../shared/shadcn/components/ui/badge.tsx';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from '../../../shared/shadcn/components/ui/dropdown-menu.tsx';
+import { TableCell, TableRow } from '../../../shared/shadcn/components/ui/table.tsx';
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
+
+
+
+/**
+ * User Row Component
+ * Component in charge of display the user's details and the actions menu.
+ */
+const UserRow = () => {
+  const a = '';
+  return (
+    <TableRow>
+      <TableCell>
+        <p className='mr-2 font-bold'>root</p>
+      </TableCell>
+      <TableCell>
+        <Badge variant='secondary'>5</Badge>
+      </TableCell>
+      <TableCell>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant='ghost' size='sm' className='max-w-20 md:max-w-24 lg:max-w-32 xl:max-w-36 2xl:max-w-40'>
+              <p className='text-ellipsis overflow-hidden font-normal'>6234ca63-426e-472c-9440-5e0b30640a5f</p>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Click to copy</p>
+          </TooltipContent>
+        </Tooltip>
+      </TableCell>
+      <TableCell>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant='ghost' size='sm' className='max-w-20 md:max-w-24 lg:max-w-32 xl:max-w-36 2xl:max-w-40'>
+              <p className='text-ellipsis overflow-hidden font-normal'>PJWC6KIPJUWHMAKC</p>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Click to copy</p>
+          </TooltipContent>
+        </Tooltip>
+      </TableCell>
+      <TableCell>
+        <DropdownMenu>
+          <DropdownMenuTrigger aria-label='User actions menu'><EllipsisVertical aria-hidden='true'/></DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>root</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><UserPen aria-hidden='true' className='w-5 h-5 mr-1' /> Update nickname</DropdownMenuItem>
+            <DropdownMenuItem><UserPen aria-hidden='true' className='w-5 h-5 mr-1' /> Update authority</DropdownMenuItem>
+            <DropdownMenuItem><UserPen aria-hidden='true' className='w-5 h-5 mr-1' /> Update OTP secret</DropdownMenuItem>
+            <DropdownMenuItem><UserMinus aria-hidden='true' className='w-5 h-5 mr-1' /> Delete user</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><KeyRound aria-hidden='true' className='w-5 h-5 mr-1' /> Display auth sessions</DropdownMenuItem>
+            <DropdownMenuItem><SquareAsterisk aria-hidden='true' className='w-5 h-5 mr-1' /> Display password updates</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export default UserRow;
