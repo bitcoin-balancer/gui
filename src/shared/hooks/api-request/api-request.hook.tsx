f7c3cd799ecd6d0f3bbee8c2e35366f432af485a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-07-29 08:58:06 -0400

    Implement the API Request Hook - ready for testing
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ src/shared/hooks/api-request/api-request.hook.tsx
@@ -0,0 +1,63 @@
+import { useState, useCallback, useEffect } from 'react';
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
+
+/**
+ * API Request Hook
+ * Sends requests to the API and handles the responses. It takes a function and an optional list
+ * of arguments that will be used to call the function with.
+ */
+const useAPIRequest = (
+  requestFunction: (...args: unknown[]) => Promise<unknown>,
+  args?: unknown[],
+) => {
+  const [data, setData] = useState<unknown | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown | undefined>(undefined);
+
+
+
+  const buildRequestFunction = useCallback(
+    () => (args === undefined ? requestFunction() : requestFunction(...args)),
+    [requestFunction, args],
+  );
+
+
+
+  useEffect(() => {
+    let ignore = false;
+
+    const executeRequest = async () => {
+      try {
+        const responseData = await buildRequestFunction();
+        setError(undefined);
+        if (!ignore) {
+          setData(responseData);
+        }
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    executeRequest();
+
+    return () => { ignore = true; };
+  }, [buildRequestFunction]);
+
+
+
+  return { data, loading, error };
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export default useAPIRequest;
