96b88beffed3c107bd33eea1fe6b4c3f0e37e06d
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-07-30 18:56:38 -0400

    The error component now extracts the error message instead of displaying a generic message
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/error/index.component.tsx
+++ src/pages/error/index.component.tsx
@@ -1,3 +1,5 @@
+import { useRouteError } from 'react-router-dom';
+import { extractMessage } from 'error-message-utils';
 import { SWService } from 'sw-service';
 import { Button } from '../../shared/shadcn/components/ui/button.tsx';
 
@@ -9,19 +11,22 @@ import { Button } from '../../shared/shadcn/components/ui/button.tsx';
  * Error Component
  * Component in charge of handling errors that can occur when loading routes.
  */
-const Error = () => (
-  <main className='h-dvh bg-primary flex justify-center items-center text-white animate-in fade-in duration-700'>
+const Error = () => {
+  const error = useRouteError();
+  return (
+    <main className='h-dvh bg-primary flex justify-center items-center text-white animate-in fade-in duration-700'>
 
-    <section className='text-center p-5 w-full sm:w-7/12 md:w-5/12 lg:w-4/12 xl:w-3/12'>
+      <section className='text-center p-5 w-full sm:w-7/12 md:w-5/12 lg:w-4/12 xl:w-3/12'>
 
-      <h1 className="text-7xl mt-5">Oops!</h1>
-      <p className='mt-5'>There was an error while processing your request. Please refresh the app and try again</p>
-      <Button size='lg' className='mt-5' onClick={SWService.updateApp}>Refresh app</Button>
+        <h1 className="text-7xl mt-5">Oops!</h1>
+        <p className='mt-5'>{extractMessage(error)}</p>
+        <Button size='lg' className='mt-5' onClick={SWService.updateApp}>Refresh app</Button>
 
-    </section>
+      </section>
 
-  </main>
-);
+    </main>
+  );
+};
 
 
 
