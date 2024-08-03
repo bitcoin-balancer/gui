a551b0b1e588b9f3c216a62de64ea3c6ffade887
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-08-03 12:08:48 -0400

    Implement APIErrorService
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ src/shared/backend/api-error/index.service.ts
@@ -0,0 +1,107 @@
+import { APIService } from '../api/index.service.ts';
+import { IAPIErrorService, IAPIError } from './types.ts';
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
+
+/**
+ * API Error Service Factory
+ * Generates the object in charge of managing the storage and retrieval of errors thrown within
+ * the API.
+ * @returns IAPIErrorService
+ */
+const apiErrorServiceFactory = (): IAPIErrorService => {
+  /* **********************************************************************************************
+   *                                          PROPERTIES                                          *
+   ********************************************************************************************** */
+
+  // ...
+
+
+
+
+
+  /* **********************************************************************************************
+   *                                           ACTIONS                                            *
+   ********************************************************************************************** */
+
+  /**
+   * Retrieves a series of API Errors. If the startAtID is provided, it will start at that point
+   * exclusively.
+   * @param limit
+   * @param startAtID?
+   * @returns Promise<IAPIError[]>
+   * @throws
+   * - 1000: if the startAtID was provided and is not a valid identifier
+   * - 1001: if the query limit is larger than the limit
+   */
+  const list = (
+    limit: number,
+    startAtID?: number,
+  ): Promise<IAPIError[]> => {
+    let urlPath: string = `api-error?limit=${limit}`;
+    if (typeof startAtID === 'number') {
+      urlPath += `&startAtID=${startAtID}`;
+    }
+    return APIService.request(
+      'GET',
+      urlPath,
+      undefined,
+      true,
+    ) as Promise<IAPIError[]>;
+  };
+
+  /**
+   * Deletes all the API Errors from the Database.
+   * @param otpToken
+   * @returns Promise<void>
+   */
+  const deleteAll = async (otpToken: string): Promise<void> => APIService.request(
+    'DELETE',
+    'api-error',
+    undefined,
+    true,
+    otpToken,
+  ) as Promise<void>;
+
+
+
+
+
+  /* **********************************************************************************************
+   *                                         MODULE BUILD                                         *
+   ********************************************************************************************** */
+  return Object.freeze({
+    // properties
+    // ...
+
+    // actions
+    list,
+    deleteAll,
+  });
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                        GLOBAL INSTANCE                                         *
+ ************************************************************************************************ */
+const APIErrorService = apiErrorServiceFactory();
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export {
+  // service
+  APIErrorService,
+
+  // types
+  type IAPIError,
+};
---
+++ src/shared/backend/api-error/types.ts
@@ -0,0 +1,83 @@
+import { IRecord } from '../../types.ts';
+
+/* ************************************************************************************************
+ *                                            SERVICE                                             *
+ ************************************************************************************************ */
+
+/**
+ * API Error Service
+ * Object in charge of managing the storage and retrieval of errors thrown within the API.
+ */
+type IAPIErrorService = {
+  // properties
+  // ...
+
+  // actions
+  list: (limit: number, startAtID?: number) => Promise<IAPIError[]>;
+  deleteAll: (otpToken: string) => Promise<void>;
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                             TYPES                                              *
+ ************************************************************************************************ */
+
+/**
+ * API Error Origin
+ * The invocation that triggered the error. It can be a specific function or a route.
+ */
+type IAPIErrorOrigin = 'AutomatedTest' | 'Notification.broadcast' | 'APIErrorRouter.get'
+| 'APIErrorRouter.delete' | 'PingRouter.get' | 'AltchaRouter.get' | 'IPBlacklistRouter.get'
+| 'IPBlacklistRouter.post' | 'IPBlacklistRouter.put' | 'IPBlacklistRouter.delete'
+| 'UserRouter.get' | 'UserRouter.get.otp-secret' | 'UserRouter.get.password-updates'
+| 'UserRouter.post' | 'UserRouter.patch.nickname' | 'UserRouter.patch.authority'
+| 'UserRouter.patch.password' | 'UserRouter.patch.otpSecret' | 'UserRouter.delete' | 'JWTRouter.get'
+| 'JWTRouter.post.sign-in' | 'JWTRouter.post.refresh-jwt' | 'JWTRouter.post.sign-out'
+| 'VersionService.initialize.__buildVersion' | 'ServerRouter.get' | 'ServerRouter.get.alarms'
+| 'ServerRouter.put.alarms' | 'ServerService.initialize.__refetchState' | 'DatabaseRouter.get.summary';
+
+/**
+ * API Error
+ * The record of the error that is stored in the database.
+ */
+type IAPIError = {
+  // the autoincrementing integer used to identify a record
+  id: number;
+
+  // the origin of the error
+  origin: IAPIErrorOrigin;
+
+  // the error message (encoded in case it was generated w/ error-message-utils)
+  error: string;
+
+  // the timestamp when the error was thrown
+  event_time: number;
+
+  // the identifier of the user who sent the request
+  uid: string | null;
+
+  // the ip address of the user who sent the request
+  ip: string | null;
+
+  // the arguments used when the error was thrown
+  args: IRecord<unknown> | null;
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export type {
+  // service
+  IAPIErrorService,
+
+  // types
+  IAPIErrorOrigin,
+  IAPIError,
+};
