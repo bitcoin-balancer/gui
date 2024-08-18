d43eba7a2a84882630ef19868fc05d73f4c3114a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-08-18 13:21:02 -0400

    Implement the form-label-with-more-info component
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ src/shared/components/form-label-with-more-info/index.component.tsx
@@ -0,0 +1,57 @@
+import { CircleHelp } from 'lucide-react';
+import { FormLabel } from '@/shared/shadcn/components/ui/form.tsx';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/shared/shadcn/components/ui/tooltip.tsx';
+import { useBoundStore } from '@/shared/store/index.store.ts';
+import { IComponentProps } from '@/shared/components/form-label-with-more-info/types.ts';
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
+
+/**
+ * Form Label With More Info Component
+ * Component in charge of ...
+ */
+const FormLabelWithMoreInfo = ({ label, description }: IComponentProps) => {
+  /* **********************************************************************************************
+   *                                             STATE                                            *
+   ********************************************************************************************** */
+  const openInfoDialog = useBoundStore((state) => state.openInfoDialog);
+
+
+
+
+
+  /* **********************************************************************************************
+   *                                           COMPONENT                                          *
+   ********************************************************************************************** */
+  return (
+    <FormLabel className='flex justify-start items-center'>
+      {label}
+      <span className='flex-1'></span>
+      <Tooltip>
+        <TooltipTrigger
+          className='w-5 h-5'
+          type='button'
+          aria-label='View more information'
+          onClick={() => openInfoDialog({ title: label, content: description })}
+        >
+          <CircleHelp
+            className='w-5 h-5'
+            aria-hidden='true'
+          />
+        </TooltipTrigger>
+        <TooltipContent side='left'><p>More info</p></TooltipContent>
+      </Tooltip>
+    </FormLabel>
+  );
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export default FormLabelWithMoreInfo;
