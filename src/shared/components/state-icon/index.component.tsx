d12bedc828b7fc619e1737338d77145bab35c9c5
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-08-19 11:08:14 -0400

    Implement the StateIcon Component
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/shared/components/state-icon/index.component.tsx
+++ src/shared/components/state-icon/index.component.tsx
@@ -1,4 +1,11 @@
-import { useState } from 'react';
+import {
+  MoveUpRight,
+  TrendingUp,
+  MoveRight,
+  TrendingDown,
+  MoveDownRight,
+} from 'lucide-react';
+import { ColorService } from '@/shared/services/color/index.service.ts';
 import { IComponentProps } from '@/shared/components/state-icon/types.ts';
 
 /* ************************************************************************************************
@@ -10,52 +17,20 @@ import { IComponentProps } from '@/shared/components/state-icon/types.ts';
  * Component in charge of displaying the icon corresponding to the current state.
  */
 const StateIcon = ({ state, className }: IComponentProps) => {
-  /* **********************************************************************************************
-   *                                             REFS                                             *
-   ********************************************************************************************** */
-
-
-
-  /* **********************************************************************************************
-   *                                             STATE                                            *
-   ********************************************************************************************** */
-  const [active, setActive] = useState();
-
-
-
-
-  /* **********************************************************************************************
-   *                                       REACTIVE VALUES                                        *
-   ********************************************************************************************** */
-
-
-
-
-
-  /* **********************************************************************************************
-   *                                         SIDE EFFECTS                                         *
-   ********************************************************************************************** */
-
-
-
-
-
-  /* **********************************************************************************************
-   *                                        EVENT HANDLERS                                        *
-   ********************************************************************************************** */
-
-
-
-
-
-  /* **********************************************************************************************
-   *                                           COMPONENT                                          *
-   ********************************************************************************************** */
-  return (
-    <>
-
-    </>
-  );
+  const classNames = `w-4 h-4${typeof className === 'string' ? ` ${className}` : ''}`;
+  if (state === 2) {
+    return <MoveUpRight aria-hidden='true' className={`${classNames} ${ColorService.INCREASE_2}`} />;
+  }
+  if (state === 1) {
+    return <TrendingUp aria-hidden='true' className={`${classNames} ${ColorService.INCREASE_1}`} />;
+  }
+  if (state === -1) {
+    return <TrendingDown aria-hidden='true' className={`${classNames} ${ColorService.DECREASE_1}`} />;
+  }
+  if (state === -2) {
+    return <MoveDownRight aria-hidden='true' className={`${classNames} ${ColorService.DECREASE_2}`} />;
+  }
+  return <MoveRight aria-hidden='true' className={`${classNames} ${ColorService.STATELESS}`} />;
 };
 
 
