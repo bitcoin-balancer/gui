c1f72fd10faddfb2bb60684c126ca83dcbc2e969
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-09-18 16:15:47 -0400

    Initialize Transaction Module
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ src/shared/backend/position/transaction/index.service.ts
@@ -0,0 +1,113 @@
+import { APIService } from '@/shared/backend/api/index.service.ts';
+import {
+  ITransactionService,
+  ITransaction,
+  ITransactionLog,
+} from '@/shared/backend/position/transaction/types.ts';
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
+
+/**
+ * Transaction Service Factory
+ * Generates the object in charge of ensuring that position actions get executed properly.
+ * @returns ITransactionService
+ */
+const transactionServiceFactory = (): ITransactionService => {
+  /* **********************************************************************************************
+   *                                          PROPERTIES                                          *
+   ********************************************************************************************** */
+
+  // ...
+
+
+
+
+
+  /* **********************************************************************************************
+   *                                          RETRIEVERS                                          *
+   ********************************************************************************************** */
+
+  /**
+   * Retrieves a transaction record from the database.
+   * @param id
+   * @returns Promise<ITransaction>
+   * @throws
+   * - 32500: if the identifier is invalid
+   * - 32000: if the tx is not found in the db
+   */
+  const getTransaction = (id: number): Promise<ITransaction> => APIService.request(
+    'GET',
+    `position/transaction/${id}`,
+    undefined,
+    true,
+  ) as Promise<ITransaction>;
+
+  /**
+   * Retrieves a series of transactions. If the startAtID is provided, it will start at that point
+   * exclusively.
+   * @param limit
+   * @param startAtID?
+   * @returns Promise<ITransaction[]>
+   * @throws
+   * - 32501: if the query limit is larger than the limit
+   * - 32502: if the startAtID was provided and is not a valid identifier
+   */
+  const listTransactions = (
+    limit: number,
+    startAtID?: number,
+  ): Promise<ITransaction[]> => {
+    let urlPath: string = `position/transactions?limit=${limit}`;
+    if (typeof startAtID === 'number') {
+      urlPath += `&startAtID=${startAtID}`;
+    }
+    return APIService.request(
+      'GET',
+      urlPath,
+      undefined,
+      true,
+    ) as Promise<ITransaction[]>;
+  };
+
+
+
+
+
+  /* **********************************************************************************************
+   *                                         MODULE BUILD                                         *
+   ********************************************************************************************** */
+  return Object.freeze({
+    // properties
+    // ...
+
+    // retrievers
+    getTransaction,
+    listTransactions,
+  });
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                        GLOBAL INSTANCE                                         *
+ ************************************************************************************************ */
+const TransactionService = transactionServiceFactory();
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export {
+  // service
+  TransactionService,
+
+  // types
+  type ITransaction,
+  type ITransactionLog,
+};
---
+++ src/shared/backend/position/transaction/types.ts
@@ -0,0 +1,114 @@
+import { ISide, IBalances } from '@/shared/backend/exchange/index.service';
+
+/* ************************************************************************************************
+ *                                            SERVICE                                             *
+ ************************************************************************************************ */
+
+/**
+ * Transaction Service
+ * Object in charge of ensuring that position actions get executed properly.
+ */
+type ITransactionService = {
+  // properties
+  // ...
+
+  // retrievers
+  getTransaction: (id: number) => Promise<ITransaction>;
+  listTransactions: (limit: number, startAtID?: number) => Promise<ITransaction[]>;
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                             TYPES                                              *
+ ************************************************************************************************ */
+
+/**
+ * Transaction Status
+ * The state of the transaction. If 'FAILED' it means it ran out of attempts and won't try again.
+ */
+type ITransactionStatus = 'PROCESSING' | 'SUCCEEDED' | 'FAILED';
+
+/**
+ * Transaction Action Name
+ * The steps that must be checked for a transaction to be considered successful.
+ */
+type ITransactionActionName = 'INITIAL_BALANCES' | 'EXECUTION' | 'CONFIRMATION';
+
+/**
+ * Transaction Log
+ * Object containing all the data related to the event that took place.
+ */
+type ITransactionLog = {
+  // the log that's being stored
+  action: ITransactionActionName;
+
+  // the time at which the log was recorded
+  eventTime: number;
+
+  // if true, the action was executed successfully
+  outcome: boolean;
+
+  // the data that was received when the event was executed
+  payload?: Record<string, unknown> | IBalances;
+
+  // the error message if the event failed to execute
+  error?: string;
+};
+
+/**
+ * Transaction
+ * The object containing the tx's details as well as the timeline
+ */
+type ITransaction = {
+  // the identifier of the tx
+  id: number;
+
+  // the timestamp in ms at which the transaction was started
+  event_time: number;
+
+  // the state of the tx
+  status: ITransactionStatus;
+
+  // the kind of action that will be executed
+  side: ISide;
+
+  // the amount of base asset that will be purchased or sold
+  amount: number;
+
+  // the list of logs generated when executing the tx
+  logs: ITransactionLog[];
+};
+
+/**
+ * Transaction Action Result
+ * The result of executing an action. Regardless of the outcome, the logs must be stored.
+ */
+type ITransactionActionResult = {
+  // the list of logs generated during the execution of the action
+  logs: ITransactionLog[];
+
+  // if it exists, the action failed to execute
+  error?: string;
+};
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export type {
+  // service
+  ITransactionService,
+
+  // types
+  ITransactionStatus,
+  ITransactionActionName,
+  ITransactionLog,
+  ITransaction,
+  ITransactionActionResult,
+};
