96f85990b172c33fd30d85eca2f708a7879e7fc2
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-09-20 18:17:08 -0400

    Decrease liquidity's default maxDistanceFromPrice to 0.05%
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/app/adjustments/liquidity.component.tsx
+++ src/pages/app/adjustments/liquidity.component.tsx
@@ -213,7 +213,7 @@ const Liquidity = ({ closeDialog }: IFormProps) => {
                   <FormControl>
                     <Input
                       type='number'
-                      placeholder='0.25'
+                      placeholder='0.05'
                       {...field}
                       autoComplete='off'
                       disabled={isSubmitting}
