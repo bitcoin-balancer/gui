86e748d58981c50e8e1a3657a4e1a223159ec363
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-09-28 10:27:25 -0400

    Initialize the strategy info dialog
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/shared/components/large-info-dialog/index.component.tsx
+++ src/shared/components/large-info-dialog/index.component.tsx
@@ -12,6 +12,7 @@ import Window from '@/shared/components/large-info-dialog/window.component.tsx';
 import Liquidity from '@/shared/components/large-info-dialog/liquidity.component.tsx';
 import Coins from '@/shared/components/large-info-dialog/coins.component.tsx';
 import Reversal from '@/shared/components/large-info-dialog/reversal.component.tsx';
+import Strategy from '@/shared/components/large-info-dialog/strategy.component.tsx';
 import Terms from '@/shared/components/large-info-dialog/terms.component.tsx';
 import { ISectionID, ISections } from '@/shared/components/large-info-dialog/types.ts';
 
@@ -43,7 +44,7 @@ const SECTIONS: ISections = {
   },
   strategy: {
     title: 'Strategy',
-    description: '',
+    description: 'Module used by Balancer to manage positions',
   },
   terms: {
     title: 'Terms',
@@ -117,6 +118,7 @@ const LargeInfoDialog = ({ data }: { data: ISectionID }) => {
         { data === 'liquidity' && <Liquidity />}
         { data === 'coins' && <Coins />}
         { data === 'reversal' && <Reversal />}
+        { data === 'strategy' && <Strategy />}
         { data === 'terms' && <Terms />}
 
       </DialogContent>
---
+++ src/shared/components/large-info-dialog/strategy.component.tsx
@@ -0,0 +1,46 @@
+
+
+/* ************************************************************************************************
+ *                                         IMPLEMENTATION                                         *
+ ************************************************************************************************ */
+
+/**
+ * Strategy
+ * Component in charge of explaining how the strategy module works.
+ */
+const Strategy = () => (
+  <>
+    <p>
+      The Liquidity Indicator monitors the Bitcoin/Dollar order book on a pre-configured exchange
+       in real-time. By analyzing and normalizing order book data, it detects unusual spikes in
+        buying or selling liquidity.
+    </p>
+    <p>
+      Once these peaks are identified on both the bid and ask sides,
+      the <strong>"Bids' dominance"</strong> is calculated. This metric reflects the relative
+       strength of buyers versus sellers.
+    </p>
+    <p>
+      The <strong>Reversal Indicator</strong> then uses this
+        dominance score to assess the current market direction and potential for a price reversal.
+    </p>
+    <img
+      src='large-info-dialog/liquidity/summary.png'
+      alt='Screenshot showing what a liquidity snapshot looks like'
+      width='852'
+      height='578'
+    />
+    <p className='text-light text-xs text-center'>
+      Example of a how the liquidity state is calculated
+    </p>
+  </>
+);
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export default Strategy;
