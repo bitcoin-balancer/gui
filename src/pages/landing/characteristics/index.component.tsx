8819d47c8afbaeb64ddb0e59460be1054d9856cf
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-09-30 11:01:51 -0400

    Complete the characteristics section
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/landing/characteristics/index.component.tsx
+++ src/pages/landing/characteristics/index.component.tsx
@@ -1,4 +1,9 @@
-
+import {
+  Bitcoin,
+  CodeXml,
+  EarthLock,
+  ShieldCheck,
+} from 'lucide-react';
 
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
@@ -10,51 +15,89 @@
  */
 const Characteristics = () => (
   <div
-      className='w-full flex justify-center items-start bg-primary text-white shadow-4'
+      className='w-full flex justify-center items-start bg-primary text-slate-50 shadow-4'
   >
     <section
-      className='w-full lg:w-11/12 xl:w-10/12 2xl:w-8/12 py-20 px-3'
+      className='w-full md:w-11/12 lg:w-9/12 xl:w-11/12 2xl:w-10/12 py-20 px-3'
     >
-      <header>
+      <header className='text-center'>
         <h2
           className='text-center text-4xl sm:text-5xl font-bold'
-        >Why choose Balancer?</h2>
+        >Why Balancer?</h2>
+        <p className='text-xs sm:text-sm mt-2 text-slate-200'>
+          Some of the reasons why you should use Balancer
+        </p>
       </header>
 
       <div
-        className='mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-between gap-10'
+        className='mt-12 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-between gap-10'
       >
-        <article>
-          <h3 className='text-white text-2xl text-center font-bold'>
+
+        {/* **************
+          * CONSERVATIVE *
+          ************** */}
+        <article className='text-center flex flex-col items-center'>
+          <Bitcoin aria-hidden='true' className='h-14 w-14' />
+
+          <h3 className='text-2xl font-bold mt-2'>
+            Conservative
+          </h3>
+
+          <p className='mt-1'>
+          Balancer employs an automated value averaging strategy that leverages market data to
+           optimize your Bitcoin holdings, aiming for long-term growth and minimizing the impact of
+            short-term volatility.
+          </p>
+        </article>
+
+        {/* *************
+          * TRANSPARENT *
+          ************* */}
+        <article className='text-center flex flex-col items-center'>
+          <CodeXml aria-hidden='true' className='h-14 w-14' />
+
+          <h3 className='text-2xl font-bold mt-2'>
             Transparent
           </h3>
 
-          <p className='text-white text-center mt-2'>
+          <p className='mt-1'>
             Balancer's code is completely <strong>open source</strong>, allowing anyone to inspect
              and audit it for security and transparency. This ensures that the platform is built on
-              a foundation of trust and accountability
+              a foundation of trust, stability and accountability.
           </p>
         </article>
-        <article>
-          <h3 className='text-white text-2xl text-center font-bold'>
+
+        {/* *********
+          * PRIVATE *
+          ********* */}
+        <article className='text-center flex flex-col items-center'>
+          <EarthLock aria-hidden='true' className='h-14 w-14' />
+
+          <h3 className='text-2xl font-bold mt-2'>
             Private
           </h3>
 
-          <p className='text-white text-center mt-2'>
+          <p className='mt-1'>
             Balancer is <strong>self-hosted</strong>, meaning you control your data and are not
              reliant on a centralized third party. This fosters greater privacy and security, as
-              your data is not stored on servers accessible by others
+              your data is not stored on servers accessible by others.
           </p>
         </article>
-        <article>
-          <h3 className='text-white text-2xl text-center font-bold'>
+
+        {/* ********
+          * SECURE *
+          ******** */}
+        <article className='text-center flex flex-col items-center'>
+          <ShieldCheck aria-hidden='true' className='h-14 w-14' />
+
+          <h3 className='text-2xl font-bold mt-2'>
             Secure
           </h3>
 
-          <p className='text-white text-center mt-2'>
+          <p className='mt-1'>
             Balancer employs advanced security protocols, undergoes regular security audits, and
              maintains a dedicated development team to address vulnerabilities and ensure ongoing
-              platform stability
+              platform stability.
           </p>
         </article>
       </div>
