f442d89a8a2139f52322e160c08fc1205441325e
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-12-21 13:33:50 -0400

    Complete initial implementation of the PositionSample Component
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/landing/position-sample/index.component.tsx
+++ src/pages/landing/position-sample/index.component.tsx
@@ -16,48 +16,54 @@ const PositionSample = memo(({ breakpoint }: { breakpoint: IBreakpoint }) => (
     className='w-full flex justify-center items-start'
   >
     <section
-      className='w-full md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12 relative'
+      className='w-full md:w-11/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12'
     >
 
-      {/* ********
-        * HEADER *
-        ******** */}
-      <header
-        className='w-full absolute z-10'
+      <div
+        className='w-full flex flex-col md:flex-row justify-center items-center gap-4 relative'
       >
-        <div
-          className='flex flex-col gap-3 sm:flex-row justify-center items-center'
+
+        <aside
+          className='w-full text-center md:text-left p-3 z-10'
         >
-          <p
+          <h2
             className='text-4xl sm:text-5xl font-bold'
-          ><strong className='text-increase-1 font-extrabold'>Buy</strong> the dip{breakpoint !== 'xs' ? '.' : ''}</p>
-          <p
-            className='text-4xl sm:text-5xl font-bold'
-          ><strong className='text-decrease-1 font-extrabold'>Sell</strong> the rally</p>
-        </div>
-      </header>
+          >
+            <strong className='text-increase-1'>Buy</strong> the dip
+          </h2>
+          <h2
+            className='text-4xl sm:text-5xl font-bold mt-3'
+          >
+            <strong className='text-decrease-1'>Sell</strong> the rally
+          </h2>
+          <p className='mt-5 text-lg'>
+            Navigate Bitcoin's volatility with your own Balancer instance, enjoying top-notch
+             security and complete privacy.
+          </p>
+          <p className='text-xs sm:text-sm mt-2 text-light'>
+          Trade automatically via your preferred exchange.
+          </p>
+        </aside>
 
 
+        <article
+          className='w-full 2xl:w-10/12 px-5 sm:px-0 -mt-20'
+        >
+          <CandlestickChart
+            height={breakpoint === 'xs' || breakpoint === 'sm' ? 400 : 500}
+            data={DATA}
+            markers={MARKERS}
+            showAttributionLogo={false}
+            hideTimeScale={true}
+            hideRightPriceScale={true}
+            hideCrosshair={true}
+            hidePriceLine={true}
+            disableScrollHandler={true}
+            disableScaleHandler={true}
+          />
+        </article>
 
-      {/* *******
-        * CHART *
-        ******* */}
-      <article
-        className='mt-3 sm:-mt-5'
-      >
-        <CandlestickChart
-          height={breakpoint === 'xs' || breakpoint === 'sm' ? 400 : 500}
-          data={DATA}
-          markers={MARKERS}
-          showAttributionLogo={false}
-          hideTimeScale={true}
-          hideRightPriceScale={true}
-          hideCrosshair={true}
-          hidePriceLine={true}
-          disableScrollHandler={true}
-          disableScaleHandler={true}
-        />
-      </article>
+      </div>
 
     </section>
   </div>
