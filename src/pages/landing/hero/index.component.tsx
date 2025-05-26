33d7c652a40de2b4055c3eefc4e097ea243020ac
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-05-26 18:57:00 -0400

    Animate hero section on the landing page
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/landing/hero/index.component.tsx
+++ src/pages/landing/hero/index.component.tsx
@@ -23,7 +23,7 @@ const Hero = memo(({
   >
 
     <div
-      className='text-center text-slate-50 p-5 w-full sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-6/12'
+      className='text-center text-slate-50 p-5 w-full sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-6/12 animate-in fade-in duration-700'
     >
 
       <h1
